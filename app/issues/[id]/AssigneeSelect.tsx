070650b5b61377e4597d15bc0fe7a0b44f4538e4
Arjun <6112arjun@gmail.com>
2023-11-07 18:41:15 +0530

    Added toast and a bit of refactoring

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,43 +1,60 @@
 "use client";
-import { User } from "@prisma/client";
+import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
+import toast, { Toaster } from "react-hot-toast";
 
-const AssigneeSelect = () => {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: async () =>
-      axios.get<User[]>("/api/users").then((res) => res.data),
-    staleTime: 1000 * 60 * 3,
-    retry: 3,
-  });
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
+  const assignIssue = (userId: string) => {
+    axios
+      .patch("/api/issues/" + issue.id, {
+        assignedToUserId: userId === "unassigned" ? null : userId,
+      })
+      .catch((err) => {
+        toast.error("Changes could not be saved");
+      });
+  };
+
+  const { data: users, error, isLoading } = useUsers();
 
   if (isLoading) return <Skeleton />;
 
   if (error) return null;
 
   return (
-    <Select.Root>
-      <Select.Trigger placeholder="Assign..."></Select.Trigger>
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          {users?.map((user) => (
-            <Select.Item key={user.id} value={user.id}>
-              {user.name}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        defaultValue={issue.assignedToUserId || ""}
+        onValueChange={assignIssue}
+      >
+        <Select.Trigger placeholder="Assign..."></Select.Trigger>
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value="unassigned">Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
   );
 };
+
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: async () =>
+      axios.get<User[]>("/api/users").then((res) => res.data),
+    staleTime: 1000 * 60 * 5,
+    retry: 3,
+  });
 
 export default AssigneeSelect;
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,6 +1,17 @@
 import { z } from "zod";
 
 export const issueSchema = z.object({
-  title: z.string().min(1).max(255),
+  title: z.string().min(1, "Title is required").max(255),
   desc: z.string().min(1),
+});
+
+export const patchIssueSchema = z.object({
+  title: z.string().min(1).max(255).optional(),
+  desc: z.string().min(1).max(65535).optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "Assigned field is required ")
+    .max(255)
+    .optional()
+    .nullable(),
 });
